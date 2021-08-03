fd46d5cdb4c9ececd8c5937dfb6997a5dcd6a5c4
Onur Yönt <onur.yont@gmail.com>
2021-08-03 12:17:49 +0300

    Add project files.

---
+++ FizzBuzzKata.Tests/FizzBuzzTest.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text; 
+using FluentAssertions;
+
+namespace FizzBuzzKata.Tests
+{
+    [TestFixture]
+    public class FizzBuzzTest
+    { 
+        private FizzBuzz _sut;
+
+        [SetUp]
+        public void TestSetUp()
+        {
+         _sut = new FizzBuzz();
+         
+        }
+         
+        [Test]
+        public void FizzBuzz_Should_PrintsOutTheNumbers1Through100()
+        {   
+            _sut.PrintList().Count.Should().Be(100);
+        }
+
+        [TestCase(3)]
+        [TestCase(6)]
+        [TestCase(9)]
+        public void FizzBuzz_Should_PrintFizz_WhenNumberDivisibleBy3(int givenNumber)
+        {  
+            var result = _sut.GenerateFizzBuzz(givenNumber);
+
+            result.Should().Be("Fizz");
+        }
+         
+        [TestCase(5)]
+        [TestCase(10)] 
+        public void FizzBuzz_Should_PrintFizz_WhenNumberDivisibleBy5(int givenNumber)
+        {  
+            var result = _sut.GenerateFizzBuzz(givenNumber);
+
+            result.Should().Be("Buzz");
+        }
+
+        [TestCase(15)]
+        [TestCase(30)]
+        [TestCase(45)]
+        public void FizzBuzz_Should_PrintFizz_WhenNumberDivisibleBy3And5(int givenNumber)
+        {
+            var result = _sut.GenerateFizzBuzz(givenNumber);
+
+            result.Should().Be("PrintList");
+        }
+
+    }
+}
---
+++ FizzBuzzKata.Tests/Generator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace FizzBuzzKata.Tests
+{
+    public class FizzBuzz
+    {
+        public List<string> PrintList()
+        {
+            var strings = new List<string>();
+            for (int i = 1; i <= 100; i++)
+            { 
+                strings.Add(GenerateFizzBuzz(i)); 
+            }
+
+            return strings;
+        }
+
+
+        public string GenerateFizzBuzz(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+                return "PrintList";
+
+            if (number % 3 == 0)
+                return "Fizz";
+
+            if (number % 5 == 0)
+                return "Buzz";
+
+            return number.ToString();
+        }
+    }
+}
